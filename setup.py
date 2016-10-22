6101af7214b2629e78a2ee2fd3c5efa74bbfa98e
Jozef <Jozef@192.168.1.9>
2016-10-22 19:52:43 -0400

    Added wheel builds and classifiers for PyPI

--- setup.py
+++ setup.py
@@ -14,6 +14,20 @@ setup(
     description = 'Compare installed Python package versions with PyPI',
     license = 'MIT',
     keywords = 'requirements check compare installed virtualenv venv pypi package packages version versions',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
     url = 'https://github.com/jaleskovec/reqcheck',
     entry_points = {
         'console_scripts': [
